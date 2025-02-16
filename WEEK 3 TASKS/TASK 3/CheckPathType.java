f798c0066793e8660ff6e526e7bfe4105bc5aa7a
THARIK-ASAN2003 <140044230+THARIK-ASAN2003@users.noreply.github.com>
2025-02-16 14:08:44 +0530

    Check path Type
    To check if the given pathname is a
    directory or a file.

---
+++ WEEK 3 TASKS/TASK 3/CheckPathType.java
@@ -0,0 +1,15 @@
+import java.io.File;
+
+public class CheckPathType {
+    public static void main(String[] args) {
+        String pathname = "C:/example/test.txt";
+        File path = new File(pathname);
+        if (path.isDirectory()) {
+            System.out.println(pathname + " is a directory");
+        } else if (path.isFile()) {
+            System.out.println(pathname + " is a file");
+        } else {
+            System.out.println(pathname + " is neither a file nor a directory");
+        }
+    }
+}
