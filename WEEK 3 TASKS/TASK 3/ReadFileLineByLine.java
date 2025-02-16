1e5882d1c6ecff485590245f78b4d74e95e163ac
THARIK-ASAN2003 <140044230+THARIK-ASAN2003@users.noreply.github.com>
2025-02-16 19:33:29 +0530

    ReadFileLineByLine.java
    To read the contents of the file line by line

--- WEEK 3 TASKS/TASK 3/ReadFileLineByLine.java
+++ WEEK 3 TASKS/TASK 3/ReadFileLineByLine.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 
 public class ReadFileLineByLine {
     public static void main(String[] args) {
-        String pathname = "C:/example/test.txt";
+        String pathname = "C:/VS Code Programs/Java Programs /techmahindra/
+Week 3 Assignments /Task 3/MyDetails.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(pathname))) {
             String line;
             while ((line = reader.readLine()) != null) {
