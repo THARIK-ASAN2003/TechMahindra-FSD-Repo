8a5c1acf04b0b0f62a65b9c44ebc8587960a60d1
THARIK-ASAN2003 <140044230+THARIK-ASAN2003@users.noreply.github.com>
2025-02-16 20:11:23 +0530

    Update ReadPlainTextFile.java
    To Read Plain Text File

--- WEEK 3 TASKS/TASK 3/ReadPlainTextFile.java
+++ WEEK 3 TASKS/TASK 3/ReadPlainTextFile.java
@@ -4,7 +4,7 @@ import java.nio.file.Paths;
 
 public class ReadPlainTextFile {
     public static void main(String[] args) {
-        String pathname = "C:/example/test.txt";
+        String pathname = "C:/VS Code Programs/Java Programs /techmahindra/Week 3 Assignments /Task 3/MyDetails.txt";
         try {
             String content = Files.readString(Paths.get(pathname));
             System.out.println("File content: " + content);
