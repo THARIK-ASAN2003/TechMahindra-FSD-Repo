5b520ffccce369bf0965f804a01686131c250d9e
THARIK-ASAN2003 <140044230+THARIK-ASAN2003@users.noreply.github.com>
2025-02-16 20:07:23 +0530

    List Files And Directories
    To Get a list of all file/directory names in
    the given directory

--- WEEK 3 TASKS/TASK 3/ListFilesAndDirectories.java
+++ WEEK 3 TASKS/TASK 3/ListFilesAndDirectories.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class ListFilesAndDirectories {
     public static void main(String[] args) {
-        String directoryPath = "C:/example";
+        String directoryPath = "C:/VS Code Programs/Java Programs /techmahindra/Week 3 Assignments /Task 3/MyDetails.txt";
         File directory = new File(directoryPath);
         String[] contents = directory.list();
         if (contents != null) {
