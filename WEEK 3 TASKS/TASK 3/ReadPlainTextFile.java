9cb75536d7187eeec7c68d5b33468023ee4cd05a
THARIK-ASAN2003 <140044230+THARIK-ASAN2003@users.noreply.github.com>
2025-02-16 14:15:58 +0530

    Read Plain Text File
    To Read Plain Text File

---
+++ WEEK 3 TASKS/TASK 3/ReadPlainTextFile.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ReadPlainTextFile {
+    public static void main(String[] args) {
+        String pathname = "C:/example/test.txt";
+        try {
+            String content = Files.readString(Paths.get(pathname));
+            System.out.println("File content: " + content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
