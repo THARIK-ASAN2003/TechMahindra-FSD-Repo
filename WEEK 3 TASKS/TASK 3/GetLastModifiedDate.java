d31c49852f7319c0c7dc06e7de3f0f8c944690d1
THARIK-ASAN2003 <140044230+THARIK-ASAN2003@users.noreply.github.com>
2025-02-16 20:05:38 +0530

    Get Last Modified Date
    To Determine the last modified date of a
    file

--- WEEK 3 TASKS/TASK 3/GetLastModifiedDate.java
+++ WEEK 3 TASKS/TASK 3/GetLastModifiedDate.java
@@ -3,7 +3,7 @@ import java.util.Date;
 
 public class GetLastModifiedDate {
     public static void main(String[] args) {
-        String pathname = "C:/example/test.txt";
+        String pathname = "C:/VS Code Programs/Java Programs /techmahindra/Week 3 Assignments /Task 3/MyDetails.txt";
         File file = new File(pathname);
         Date lastModified = new Date(file.lastModified());
         System.out.println("Last modified: " + lastModified);
