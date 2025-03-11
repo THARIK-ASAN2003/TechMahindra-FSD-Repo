4998a12c731c480420d249a7522c2682959f8baa
H Mohamed Tharik Asan <tharikhazzlewood12032003@gmail.com>
2025-03-11 13:03:58 +0530

    Deleted old task files

--- WEEK 3 TASKS/TASK 4/ExceptionDemo.java
+++
@@ -1,9 +0,0 @@
-public class ExceptionDemo {
-    public static void main(String[] args) {
-        try {
-            throw new Exception("This is a custom exception.");
-        } catch (Exception e) {
-            System.out.println("Caught an exception: " + e.getMessage());
-        }
-    }
-}
